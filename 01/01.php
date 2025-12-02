c4b95f1803cf947ac7bfdcfc510f4b2c00c7407b
Maxime Gosselin <maxime.gosselin@gmail.com>
2025-12-01 21:11:28 -0500

    day 1 part 2

--- 01/01.php
+++ 01/01.php
@@ -4,15 +4,40 @@ declare(strict_types=1);
 
 $input = explode(PHP_EOL, trim(file_get_contents(__DIR__ . '/input.txt')));
 
-$cursor = 50;
+$position = 50;
 $zeroes = 0;
 
+$turnRight = function (int $position, int $steps) use (&$zeroes): int {
+    foreach (range(1, $steps) as $step) {
+        $position++;
+        if ($position === 100) {
+            $position = 0;
+            $zeroes++;
+        }
+    }
+    return $position;
+};
+
+$turnLeft = function (int $position, int $steps) use (&$zeroes): int {
+    foreach (range(1, $steps) as $step) {
+        $position--;
+        if ($position === 0) {
+            $zeroes++;
+        }
+        if ($position === -1) {
+            $position = 99;
+        }
+    }
+    return $position;
+};
+
 foreach ($input as $move) {
-    $dir = str_starts_with($move, 'L') ? -1 : 1;
-    $steps = $dir * (int)substr($move, 1);
-    $cursor = ($cursor + $steps) % 100;
-    if ($cursor === 0) {
-        $zeroes++;
+    $dir = substr($move, 0, 1);
+    $steps = (int)substr($move, 1);
+    if ($dir === 'L') {
+        $position = $turnLeft($position, $steps);
+    } else {
+        $position = $turnRight($position, $steps);
     }
 }
 
