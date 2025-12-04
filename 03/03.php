0a9b5875dc4981988d7e790ce14c2b1e81c34c09
Maxime Gosselin <maxime.gosselin@gmail.com>
2025-12-03 20:29:37 -0500

    03

---
+++ 03/03.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+$lines = explode(PHP_EOL, trim(file_get_contents(__DIR__ . '/input.txt')));
+
+$totalJoltage = 0;
+
+foreach ($lines as $line) {
+    $numbers = array_map('intval', str_split($line));
+    $maxNumber = max($numbers);
+    $joltage = $maxNumber;
+
+    $indexOfFirstMaxNumber = array_search($maxNumber, $numbers);
+    $numbersBefore = array_map('intval', str_split(substr($line, 0, $indexOfFirstMaxNumber)));
+    $numbersAfter = array_map('intval', str_split(substr($line, $indexOfFirstMaxNumber)));
+
+    if (count($numbersAfter)) {
+        $joltage .= max($numbersAfter);
+    } else {
+        $joltage .= max($numbersBefore);
+    }
+    $totalJoltage += intval($joltage);
+}
+
+echo $totalJoltage;
